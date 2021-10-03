96cb18b03a2f7f9bfe31809449c1c0b05b85e17e
Mike Babb <mike7400@gmail.com>
2021-10-03 17:31:31 -0400

    Initial imp

--- src/Robot.java
+++ src/Robot.java
@@ -1,4 +1,16 @@
-
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Objects;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Random;
+import java.util.Stack;
 
 /**
     Represents an intelligent agent moving through a particular room.
@@ -11,67 +23,187 @@
    env.getTargetCol()
 */
 
-public class Robot
-{
+public class Robot {
     private Environment env;
     private int posRow;
     private int posCol;
     private String searchAlgorithm;
     public int expanded = 0;
-    private int timeStep = 0;
 
-    private PathFinder pathfinder;
+    public boolean done;
+    public boolean[][] visited;
+
+    public Node startNode, targetNode;
+
+    public Map<Node, Node> nodeTree;
+    public Stack<Action> pathStack;
+
+    public static int[][] directions = {{0, 0, -1, 1}, {1, -1, 0, 0}};
+
+    public class Node implements Comparable<Node> {
+        public int x, y;
+        public Action action;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+            this.action = Action.DO_NOTHING;
+        }
+
+        public Node(int x, int y, Action action) {
+            this(x, y);
+            this.action = action;
+        }
+
+        public int compareTo(Node other) {
+            final var xCompare = Integer.compare(x, other.x);
+            final var yCompare = Integer.compare(y, other.y);
+
+            if (xCompare != 0) {
+                return xCompare;
+            } else if (yCompare != 0) {
+                return yCompare;
+            }
+
+            return 0;
+        }
+
+        public Node copy() { return new Node(this.x, this.y, this.action); }
+
+        @Override
+        public boolean equals(Object o) {
+
+            Node node = (Node)o;
+            return x == node.x && y == node.y;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+
+        public String toString() {
+            return String.format("(%s, %s); %s", this.x, this.y, this.action);
+        }
+    }
+
     /**
         Initializes a Robot on a specific tile in the environment.
     */
-    public Robot(Environment env)
-    {
-        this(env, 0, 0, "");
-    }
-    public Robot(
-        Environment env,
-        int posRow,
-        int posCol,
-        String searchAlgorithm)
-    {
+    public Robot(Environment env) { this(env, 0, 0, ""); }
+    public Robot(Environment env, int posRow, int posCol,
+                 String searchAlgorithm) {
         this.env = env;
         this.posRow = posRow;
         this.posCol = posCol;
         this.searchAlgorithm = searchAlgorithm;
 
-        this.pathfinder = new PathFinder(this.env, this.posRow, this.posCol);
+        this.startNode = new Node(posRow, posCol);
+        this.targetNode = this.findTargetNode();
+
+        this.nodeTree = new HashMap<Node, Node>();
+        this.pathStack = new Stack<Action>();
     }
-    public int getPosRow()
-    {
-        return posRow;
+
+    public int getPosRow() { return posRow; }
+    public int getPosCol() { return posCol; }
+    public void incPosRow() { posRow++; }
+    public void decPosRow() { posRow--; }
+    public void incPosCol() { posCol++; }
+    public void decPosCol() { posCol--; }
+
+    // BEGIN
+
+    public Node findTargetNode() {
+        for (int i = 0; i < env.getRows(); i++) {
+            for (int j = 0; j < env.getCols(); j++) {
+                final var status = env.getTileStatus(i, j);
+                if (status == TileStatus.TARGET) {
+                    return new Node(i, j);
+                }
+            }
+        }
+        return null;
     }
-    public int getPosCol()
-    {
-        return posCol;
+
+    public ArrayList<Node> getNeighbors(Node node) {
+        int baseRow = node.x;
+        int baseCol = node.y;
+
+        final var neighbors = new ArrayList<Node>();
+
+        for (int i = 0; i < 4; i++) {
+            final var row = baseRow + directions[0][i];
+            final var col = baseCol + directions[1][i];
+
+            if (this.env.validPos(row, col) && !this.visited[row][col]) {
+                final var neighbor = new Node(row, col);
+                neighbors.add(neighbor);
+            }
+        }
+
+        return neighbors;
     }
-    public void incPosRow()
-    {
-        posRow++;
+
+    public boolean finished(Node node) {
+        return this.env.getTileStatus(node.x, node.y) == TileStatus.TARGET;
     }
-    public void decPosRow()
-    {
-        posRow--;
+
+    public void createPath() {
+        var node = this.targetNode;
+        this.pathStack.add(this.targetNode.action);
+
+        while (node != null && !node.equals(this.startNode)) {
+            node = this.nodeTree.get(node);
+            this.pathStack.add(node.action);
+        }
+
+        Collections.reverse(this.pathStack);
     }
-    public void incPosCol()
-    {
-        posCol++;
+
+    public void DFS() {
+        final Stack<Node> S = new Stack<Node>();
+        S.add(this.startNode);
+
+        while (!S.isEmpty()) {
+            final var node = S.pop();
+
+            if (this.finished(node)) {
+                break;
+            }
+
+            final var neighbors = this.getNeighbors(node);
+
+            for (final var n : neighbors) {
+                S.push(n);
+                this.nodeTree.put(n, node.copy());
+            }
+        }
     }
-    public void decPosCol()
-    {
-        posCol--;
-    }
+
+    // END
 
     /**
      * Construct search tree before Robot start moving.
      */
-    public void plan()
-    {
-        this.pathfinder.search(searchAlgorithm);
+    public void plan() {
+        switch (searchAlgorithm) {
+        case "DFS":
+            this.DFS();
+            break;
+        case "AStar":
+            this.AStar();
+            break;
+        case "RBFS":
+            this.RBFS();
+            break;
+        case "HillClimbing":
+            this.HillClimbing();
+        default:
+            break;
+        }
+
+        this.createPath();
     }
 
     /**
@@ -79,14 +211,28 @@ public class Robot
         At each time-step, the Robot decides which direction
         to move.
     */
-    public Action getAction()
-    {
+    public Action getAction() {
         // you can get a tile's status with
         TileStatus status = env.getTileStatus(posRow, posCol);
+
         Action action = Action.DO_NOTHING;
 
-        action = this.pathfinder.path.get(this.timeStep).action;
-        this.timeStep += 1;
+        switch (searchAlgorithm) {
+        case "DFS":
+            action = this.pathStack.pop();
+            break;
+        case "AStar":
+            action = this.pathStack.pop();
+            break;
+        case "RBFS":
+            action = this.pathStack.pop();
+            break;
+        case "HillClimbing":
+            action = this.pathStack.pop();
+            break;
+        default:
+            break;
+        }
 
         return action;
     }
